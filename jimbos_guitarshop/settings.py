f6bbfe17086c18bc390e3d801202485d5960b633
Ubuntu <ubuntu@ip-172-31-43-222.ec2.internal>
2020-02-09 05:18:14 +0000

    Setup Index

--- jimbos_guitarshop/settings.py
+++ jimbos_guitarshop/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'jimbos_guitarshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+# enable static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
