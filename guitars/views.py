60db3f6ebffbce7ab72d3a70f4fd57fad7adde90
Ubuntu <ubuntu@ip-172-31-43-222.ec2.internal>
2020-02-09 06:33:29 +0000

    Added show_guitars.html

--- guitars/views.py
+++ guitars/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Guitars
 
 # Create your views here.
+
+
+def show_guitars(request):
+    all_guitars = Guitars.objects.all()
+    return render(request, 'show_guitars.html', {
+        'all_guitars':all_guitars
+    })
+
--- jimbos_guitarshop/urls.py
+++ jimbos_guitarshop/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import home
+from guitars.views import show_guitars
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('show_guitars/', show_guitars)
 ]
