b9fdb382a9939683fe6993b09ae435b05b140dfe
Ubuntu <ubuntu@ip-172-31-43-222.ec2.internal>
2020-02-10 06:54:41 +0000

    Attempt Heroku Deployment 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jimbos_guitarshop.wsgi:application
--- jimbos_guitarshop/settings.py
+++ jimbos_guitarshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f$fm(!slio31f&8sw2_sp%0!2wtm!#fg#z1ix1u7(4el@3#wui'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["jimboguitars.herokuapp.com" , "*"]
 
 
 # Application definition
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'jimbos_guitarshop.urls'
@@ -125,7 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
