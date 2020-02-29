4c35ed655aa4b95531861d4ae5347b1e513fcfe7
Ubuntu <jimmy_hsu@singnet.com.sg>
2020-02-29 06:15:53 +0000

    displayed total price in cart

--- cart/views.py
+++ cart/views.py
@@ -18,12 +18,14 @@ def add_to_cart(request, guitar_id):
             'cost': str(guitar.cost),
             'quantity':1,
             'image_url':guitar.image.cdn_url,
+            # 'subtotal':float(guitar.cost)
         }
         
         # save the cart back to sessions
         request.session['shopping_cart'] = cart
         messages.success(request, "Guitar has been added to your cart!")
     else:
+        cart['guitar_id']['cost'] = int(cart[guitar_id]['quantity']) * int(cart[guitar_id]['cost'])
         messages.success(request, "The guitar is already in your shopping cart")
     return redirect('/show_guitars/')
         
@@ -31,10 +33,35 @@ def add_to_cart(request, guitar_id):
 def view_cart(request):
     # retrieve the cart
     cart = request.session.get('shopping_cart', {})
+    total_price = 0.00
+    
+    for guitar_id,guitar in cart.items():
+        total_price += int(guitar['quantity']) * int(float(guitar['cost']))
+    print(total_price)
+    
     return render(request, 'view_cart.html', {
-        'shopping_cart':cart
+        'shopping_cart':cart,
+        'total_price':total_price
     })
 
+# def total_price(request, guitar_id):
+#     guitar = get_object_or_404(Guitars, pk=guitar_id)
+#     cart = request.session.get('shopping_cart', {})
+#     cart[guitar_id] = {
+#     # 'id':guitar_id,
+#     # 'name': game.name,
+#     # 'price': str(game.price),
+#     # 'image_url':game.image.cdn_url,
+#     'quantity':cart['quantity'],
+#     'total_price':int(cart[guitar_id]['quantity']) * float(cart[guitar_id]['price'])
+#     }
+    
+#     # print(quantity)
+#     request.session['shopping_cart'] = cart
+#     return render(request, 'view_cart.html', {
+#             'total_price':total_price
+#         }) 
+        
 def remove_from_cart(request, guitar_id):
     cart = request.session.get('shopping_cart', {})
     
@@ -45,7 +72,6 @@ def remove_from_cart(request, guitar_id):
         return redirect('/cart/') 
         
 def adjust_cart(request, guitar_id):
-    # print(request.POST)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('shopping_cart', {})
 
